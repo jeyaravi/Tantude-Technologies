2e7723055dca24f33dfae29bb8e669301014faed
Your Name <you@example.com>
2026-06-16 17:17:18 +0530

    First commit

--- greater2num.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-if num1 > num2:
-    print(num1, "is greater than", num2)
-else:
-    print(num2, "is greater than", num1)
