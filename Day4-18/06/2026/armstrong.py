0c5754592735e37b7739dfea7fdf7be1b158c3a8
jeyaravi <rjeyashree2003@gmail.com>
2026-06-18 12:38:42 +0530

    Day 4

---
+++ Day4-18/06/2026/armstrong.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+temp = num
+sum = 0
+digits = len(str(num))
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** digits
+    temp //= 10
+if sum == num : 
+    print(num, "is an Armstrong number.")
+else: 
+    print(num, "is not an Armstrong number.")
---
+++ Day4-18/06/2026/reversestring.py
@@ -0,0 +1,3 @@
+string=input("Enter a string:")
+reversed_string=string[::-1]
+print("The reversed string is:",reversed_string)
